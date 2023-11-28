350bf336cfaa333b8861986d9e89fccad2f42ef1
Pete <ppete.st10@gmail.com>
2023-11-28 20:22:05 +0700

    updates

--- Project_HTML2023/script.js
+++ Project_HTML2023/script.js
@@ -1,6 +1,5 @@
 function toggleHam(x) {
   x.classList.toggle("change");
-
   let myMenu = document.getElementById('dropdown')
   if(myMenu.className === 'dropdown-menu'){
     myMenu.className += ' dropdown-menu-active'
