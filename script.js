1b7077b4229e52f84caa2d46a3da264787e4a408
Pete <ppete.st10@gmail.com>
2023-11-28 20:19:13 +0700

    Merge branch 'main' of https://github.com/ppete10/int102-Webtech

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    
+    let myMenu = document.getElementById('')
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
