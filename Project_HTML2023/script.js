a2d794d55b414bf80a30884eb79b706c1095474a
Pete <ppete.st10@gmail.com>
2023-11-28 23:13:57 +0700

    updates

--- Project_HTML2023/script.js
+++ Project_HTML2023/script.js
@@ -7,3 +7,18 @@ function toggleHam(x) {
       myMenu.className = 'dropdown-menu'
     }
   }
+
+  var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("Slides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 6000);    
+  }
