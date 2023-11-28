c68c0d56eba34d01f0b79660012a99e031deda5e
Pete <ppete.st10@gmail.com>
2023-11-28 19:54:50 +0700

    updates

---
+++ Project_HTML2023/script.js
@@ -0,0 +1,10 @@
+function toggleHam(x) {
+  x.classList.toggle("change");
+
+  let myMenu = document.getElementById('dropdown')
+  if(myMenu.className === 'dropdown-menu'){
+    myMenu.className += ' dropdown-menu-active'
+  } else {
+    myMenu.className = 'dropdown-menu'
+  }
+}
--- script.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    
-    let myMenu = document.getElementById('')
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
